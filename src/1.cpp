ca34c50c2c5b66a8889dc51cddc9ad3518602e66
arsigatalavera <arsigatalavera@gmail.com>
2025-03-06 06:38:20 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main(){
+    cout<<"Enter a number:"<<endl;
+    double num;
+    cin>>num;
+    cout<<sqrt(num)<<endl;
+}
