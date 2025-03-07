bbb554412202b94dfb7e548b05590e4da317bb1c
arsigatalavera <arsigatalavera@gmail.com>
2025-03-07 14:56:26 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double x = 2;
+  double y = 3;
+  double z = 5;
+  cout << "The sum of the numbers is: " << x + y + z << endl;
+  return 0;
+}
