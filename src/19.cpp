9d1b1e8a337786af6b4b675baebd186a7cf50892
arsigatalavera <arsigatalavera@gmail.com>
2025-03-29 06:12:10 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    double result = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << result << endl;
+    return 0;
+}
