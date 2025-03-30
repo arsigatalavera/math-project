79d647ffd41e68eacad93a011bdbf4c989506457
arsigatalavera <arsigatalavera@gmail.com>
2025-03-30 13:04:26 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.141592653589793;
+    float z = 10.5f;
+    string s = "Hello, World!";
+    
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+    cout << "s = " << s << endl;
+
+    return 0;
+}
