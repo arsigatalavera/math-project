66bead23bfac21a8e89288427f90b926dfe585d7
arsigatalavera <arsigatalavera@gmail.com>
2025-04-06 07:56:59 +0000

    Add 26.cpp

---
+++ src/26.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    
+    if (num1 > num2) {
+        cout << "The larger number is " << num1 << endl;
+    } else {
+        cout << "The smaller number is " << num2 << endl;
+    }
+    
+    return 0;
+}
